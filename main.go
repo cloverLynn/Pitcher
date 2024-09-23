763b86592cfdf6b56fa41d2f806bb5676ab4c4ee
Clover <clover.forbes96@gmail.com>
2024-09-22 22:43:36 -0400

    Using Huh to add popup to fill out form for new media

--- main.go
+++ main.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 	"github.com/76creates/stickers/flexbox"
 	"github.com/charmbracelet/bubbletea"
+	huh "github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 	components "main/Components"
 	"main/media"
 	"os"
+)
+
+type status int
+
+var models []tea.Model
+
+const (
+	movies status = iota
+	form
+)
+
+var (
+	column   = lipgloss.NewStyle().Background(lipgloss.Color("#000000")).Align(lipgloss.Center)
+	selected = lipgloss.NewStyle().Background(lipgloss.Color("#8B0000")).Align(lipgloss.Center)
 )
 
 type Model struct {
@@ -16,6 +32,8 @@ type Model struct {
 }
 
 func (m *Model) Init() tea.Cmd { return nil }
+
+func New() *Model { return &Model{} }
 
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -33,6 +51,11 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.FlexBoxComponent.Scroll(msg.String())
 		case "enter":
 			m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected].PlayTrailer()
+		case "ctrl+n":
+			models[movies] = m
+			newForm := NewForm()
+			models[form] = newForm
+			return models[form].Update("First")
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
@@ -54,9 +77,9 @@ func (m *Model) InitializeData(width, height int) {
 
 	rows := []*flexbox.Row{
 		m.FlexBoxComponent.FlexBox.NewRow().AddCells(
-			flexbox.NewCell(1, 6).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected-1].View()),
-			flexbox.NewCell(1, 6).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected].View()),
-			flexbox.NewCell(1, 6).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected+1].View()),
+			flexbox.NewCell(1, 6).SetStyle(column).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected-1].View()),
+			flexbox.NewCell(1, 6).SetStyle(selected).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected].View()),
+			flexbox.NewCell(1, 6).SetStyle(column).SetContent(m.FlexBoxComponent.Movies[m.FlexBoxComponent.Selected+1].View()),
 		),
 	}
 	m.FlexBoxComponent.FlexBox.AddRows(rows)
@@ -66,8 +89,71 @@ func (m *Model) View() string {
 	return m.FlexBoxComponent.View()
 }
 
+type Form struct {
+	form    *huh.Form // huh.Form is just a tea.Model
+	loaded  bool
+	name    string
+	poster  string
+	trailer string
+}
+
+func NewForm() Form {
+	path, _ := os.UserHomeDir()
+	return Form{
+		form: huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().
+					Key("name").
+					Title("Media Name"),
+				huh.NewFilePicker().CurrentDirectory(path).
+					Title("Poster").
+					Key("poster"),
+				huh.NewFilePicker().CurrentDirectory(path).
+					Title("Trailer").
+					Key("trailer"),
+			)),
+	}
+}
+
+func (t Form) Init() tea.Cmd {
+	return t.form.Init()
+}
+
+func (t Form) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if !t.loaded {
+		t.form.NextField()
+		t.form.PrevField()
+		t.loaded = true
+	}
+	form, cmd := t.form.Update(msg)
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return models[movies].Update(nil)
+		case "enter":
+		}
+	}
+	if f, ok := form.(*huh.Form); ok {
+		t.form = f
+	}
+
+	return t, cmd
+}
+
+func (t Form) View() string {
+	if t.form.State == huh.StateCompleted {
+		name := t.form.GetString("name")
+		poster := t.form.GetString("poster")
+		trailer := t.form.GetString("trailer")
+		return fmt.Sprintf("You selected: %s, Lvl. %s %s", name, poster, trailer)
+	}
+	return t.form.View()
+}
+
 func main() {
-	m := &Model{}
+	models = []tea.Model{New(), NewForm()}
+	m := models[movies]
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error: %v", err)
